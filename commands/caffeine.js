048631c5b49f4111969aeafc6453186dcd899e58
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-05 02:20:44 -0500

    Update caffeine.js

--- commands/caffeine.js
+++ commands/caffeine.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-message.channel.send("Call 531-721-9216")
+message.channel.send("Go Buy Some Coffee")
  }
 
 module.exports.help = {
