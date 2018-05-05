89639bd31191fbabd8c94ac497eaf763e8457b14
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-04 22:56:47 -0500

    Create cmds.js

---
+++ commands/cmds.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+let commands = new Discord.RichEmbed()
+.setAuthor(bot.user.username, bot.user.avatarURL);
+
+message.channel.send(commands);
+ }
+
+module.exports.help = {
+  name: "ping"
+}
