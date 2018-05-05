b9acc318229154c348f92b3e1c4b3df955b3df39
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-05 00:26:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,10 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
+  
+  if(message.content.startsWith("!")) {
+  if(!message.member.roles.find("name", "gay role")) return message.channel.send("Don't Even Think About It");
+  };
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
