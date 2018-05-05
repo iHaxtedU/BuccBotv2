6fd0ab1c5e36cd65e2a5cd39a7eb5e2cbfd1e12d
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-05 02:42:21 -0500

    Update updates.js

--- commands/updates.js
+++ commands/updates.js
@@ -1,9 +1,14 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+let updates = new Discord.RichEmbed()
+.setAuthor("BuccBotv2", bot.user.avatarURL)
+.setColor("#2ef22b")
+.addField("V1 (Official Release)", "Added !updates Command =-= Added !credits Command =-= Added Onto !cmds Command")
 
+message.channel.send(updates);
  }
 
 module.exports.help = {
-  name: "caffeine"
+  name: "updates"
 }
