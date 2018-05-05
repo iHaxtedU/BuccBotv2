341a8efada1ea7f658bdbdb11e62e3e8d2ac60ac
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-05 02:37:24 -0500

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -1,1 +1,14 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+let credits = new Discord.RichEmbed()
+.setAuthor("BuccBotv2", bot.user.avatarURL)
+.setColor("#2ef22b")
+.addField("Credits", "Ez Potato#2648 Is The Creator Of The Bot");
+          
+ message.channel.send(credits);
+ }
+
+module.exports.help = {
+  name: "credits"
+}
