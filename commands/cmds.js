9a5b0a8013aa8c07de0626bc7a1f862a65e08d25
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-04 23:11:55 -0500

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -4,7 +4,8 @@ module.exports.run = async (bot, message, args) => {
 let commands = new Discord.RichEmbed()
 .setColor("#00FF00")
 .setAuthor("BuccBotv2", bot.user.avatarURL)
-.addField("!ping", "Show's You The Reponse Time For The Bot");
+.addField("!ping", "Show's You The Reponse Time For The Bot.")
+.addField("!contact", "Contact A User.");
 
 message.channel.send(commands);
  }
