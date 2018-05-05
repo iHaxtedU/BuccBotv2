16b420487200be6b52c139006a4242aecf890071
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-05 00:53:10 -0500

    Update nonfe.js

--- commands/nonfe.js
+++ commands/nonfe.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   let choices = ["Im Hoping", "That This", "Is Working"];
-  let random = Math.floor(Math.random()*choices);
+  let random = Math.floor(Math.random()*choices.length.content);
   
   message.author.send(random);
 }
