2d50be30c2f5d637285d5ff5d90a64dd7461cc86
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-07 18:52:01 -0500

    Update caffeine.js

--- commands/caffeine.js
+++ commands/caffeine.js
@@ -1,1 +1,11 @@
+var answers = [
+  "Hey",
+  "Howdy",
+  "Hello There",
+  "Wotcha",
+  "Alright gov'nor"
+]
 
+var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+message.author.send(randomAnswer);
