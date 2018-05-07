03ac98db2c8202fd8b5c037f24134e31fb43fdf1
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-07 18:51:45 -0500

    Update caffeine.js

--- commands/caffeine.js
+++ commands/caffeine.js
@@ -1,9 +1,1 @@
-const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-message.channel.send("Go Buy Some Coffee")
- }
-
-module.exports.help = {
-  name: "caffeine"
-}
