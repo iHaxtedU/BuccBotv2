558c396699288acbca6d78bc9efa27624ae42eba
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-07 18:58:26 -0500

    Update caffeine.js

--- commands/caffeine.js
+++ commands/caffeine.js
@@ -1,11 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
 var answers = [
-  "Hey",
-  "Howdy",
-  "Hello There",
-  "Wotcha",
-  "Alright gov'nor"
+  "Ahhhh Heelll Naaaahhh",
+  "Hi",
+  "Why TF Did U Use This Command You Fucking Insolent, Uncurcumsized, Gay Ass Cunt. :face_palm:",
+  "Ez Potato Is A Fucking Boss BTW Just Remember That!",
+  "Did You Know About 38% Of American Men Are Gay"
 ]
 
 var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
 
-message.author.send(randomAnswer);
+message.channel.send(randomAnswer);
+ }
+
+module.exports.help = {
+  name: "caffeine"
+}
