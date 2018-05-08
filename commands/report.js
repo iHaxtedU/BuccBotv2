36882cf2b5afb01791f71fa1fd4145258b2c08fe
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-07 20:05:31 -0500

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,1 +1,15 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+let user = message.mentions.members.first();
+if (!user) return message.channel.send("Please Report A User");
+let content = args.slice(1).join(" ");
+if (!content) return message.channel.send("You Need A Reason :face_palm:");
+                                          
+let reportEmbed = new Discord.RichEmbed()
+.setAuthor(
+ }
+
+module.exports.help = {
+  name: ""
+}
