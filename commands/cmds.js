fe86f1935f366cff4e80c28c2424de4af27c3c2b
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-07 19:46:55 -0500

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -6,8 +6,9 @@ let commands = new Discord.RichEmbed()
 .setAuthor("BuccBotv2", bot.user.avatarURL)
 .addField("!ping", "Show's You The Reponse Time For The Bot.")
 .addField("!contact", "Contact A User.")
-.addField("!credits", "View Credits For The Bot")
-.addField("!updates", "Added Updates Command For Bot");
+.addField("!credits", "View Credits For The Bot.")
+.addField("!updates", "Added Updates Command For Bot.")
+.addField("!caffeine", "Use This To Learn Something Cool.");
 
 message.channel.send(commands);
  }
