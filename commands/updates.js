3cffa0b1c42eb951acf3df4851a7828cccbe3c33
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-07 19:45:40 -0500

    Update updates.js

--- commands/updates.js
+++ commands/updates.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 let updates = new Discord.RichEmbed()
 .setAuthor("BuccBotv2", bot.user.avatarURL)
 .setColor("#2ef22b")
-.addField("V2 (Stable Release)", "Allowing Users To Now Use The Commands, Have Fun!!!)
+.addField("V2 (Stable Release)", "Allowing Users To Now Use The Commands, Have Fun!!!")
 
 message.channel.send(updates);
  }
